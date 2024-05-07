63c84085a3a9c2c2e556a56407d47a47ed56052c
AlexanderSlesarenko <slesarenkoa@yandex.ru>
2024-05-07 14:25:47 +0700

    Update client.js

--- client.js
+++ client.js
@@ -1,18 +1,10 @@
 const fs = require('node:fs')
 const { merge } = require('webpack-merge')
-// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const baseConfig = require('./base.js')
 
 const isProduction = process.env.NODE_ENV === 'production'
-
-const { PROJECT } = process.env
-
-const loader = fs.readFileSync(require.resolve(`./${PROJECT}/loader.svg`), 'utf8')
-const favicons = fs.readFileSync(require.resolve(`./${PROJECT}/favicons.txt`), 'utf8')
-const title = PROJECT.charAt(0).toUpperCase() + PROJECT.slice(1)
-const google = fs.readFileSync(require.resolve(`./${PROJECT}/google.txt`), 'utf8')
 
 module.exports = merge(baseConfig, {
   target: ['web', 'es5'],
@@ -29,14 +21,7 @@ module.exports = merge(baseConfig, {
     new HtmlWebpackPlugin({
       template: './public/index.no-ssr.html',
       filename: 'index.html',
-      inject: false,
-      templateParameters: {
-        loader,
-        favicons,
-        title,
-        google
-      }
+      inject: false
     })
-    // new BundleAnalyzerPlugin()
   ]
 })
